8bb14fc6d13ca44c793ea4fc49733a339e8309ba
AbujafarCh <357jafar@gmail.com>
2024-02-19 11:50:06 +0600

    some funtionality added

--- scripts/common.js
+++ scripts/common.js
@@ -1,11 +1,11 @@
-function seatLeft(){
+function seatLeft() {
     const allSeat = document.getElementById('seat-left')
     let allSeatInt = parseInt(allSeat.innerText)
     let seatLeft = allSeatInt - 1
     allSeat.innerText = seatLeft;
 }
 
-function createTr(seatName){
+function createTr(seatName) {
     const tr = document.createElement('tr')
     const td1 = document.createElement('td')
     td1.innerText = seatName;
@@ -18,24 +18,23 @@ function createTr(seatName){
     tr.appendChild(td1)
     tr.appendChild(td2)
     tr.appendChild(td3)
-    
+
     document.getElementById('ticket-table').appendChild(tr)
 }
 
-function countSeat(){
+function countSeat() {
     const getSeat = document.getElementById('seat-count')
     const seatInnerNumber = parseInt(getSeat.innerText)
     let seatCount = seatInnerNumber + 1
     getSeat.innerText = seatCount
-    console.log(getSeat.innerText)
 }
 
-function seatBackground(seatElement){
+function seatBackground(seatElement) {
     seatElement.style.backgroundColor = '#1DD100';
     seatElement.style.color = '#ffffff';
 }
 
-function totalPrice(){
+function totalPrice() {
     const totalPriceElement = document.getElementById('total-price')
     let totalPrice = parseInt(totalPriceElement.innerText)
     totalPrice = totalPrice + 550;
@@ -45,22 +44,43 @@ function totalPrice(){
     grandTotalElement.innerText = totalPrice;
 }
 
-// function couponAuthentication(){
-//     const couponInputElement = document.getElementById('coupon-input')
-//     const applyBtn = document.getElementById('apply')
-//     applyBtn.addEventListener('click', function(){
-//         const couponInput = couponInputElement.value
-//         if(couponInput === 'NEW15'){
-//             const totalPriceElement = document.getElementById('total-price')
-//             let totalPrice = parseInt(totalPriceElement.innerText)
-//             let grandTotal = totalPrice*15/100
-//             return grandTotal
-//         }
-//     })
-    
-// }
-couponAuthentication()
-function grandTotal(){
-    const grandTotalElement = document.getElementById('grand-total')
-    grandTotalElement.innerText = couponAuthentication()
+function enableApply() {
+    const applyBtn = document.getElementById('apply')
+    applyBtn.removeAttribute('disabled')
 }
+
+function hideApply(){
+    const applyBtn = document.getElementById('coupon-section')
+    applyBtn.classList.add('hidden')
+}
+
+
+
+const couponInputElement = document.getElementById('coupon-input')
+const applyBtn = document.getElementById('apply')
+console.log(applyBtn)
+applyBtn.addEventListener('click', function () {
+    const couponInput = couponInputElement.value
+    if (couponInput == 'NEW15') {
+        const grandTotalElement = document.getElementById('grand-total')
+        let grandTotalInt = parseInt(grandTotalElement.innerText)
+        let grandTotal = grandTotalInt * 85 / 100
+        grandTotalElement.innerText = grandTotal;
+        hideApply()
+    }
+    if (couponInput == 'Couple20') {
+        const grandTotalElement = document.getElementById('grand-total')
+        let grandTotalInt = parseInt(grandTotalElement.innerText)
+        let grandTotal = grandTotalInt * 80 / 100
+        grandTotalElement.innerText = grandTotal;
+        hideApply()
+    }
+})
+
+
+
+
+// function grandTotal(){
+//     const grandTotalElement = document.getElementById('grand-total')
+//     grandTotalElement.innerText = couponAuthentication()
+// }
--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,26 @@
-function selectSeat(){
+function selectSeat() {
+    let i = 0
     const seats = document.getElementsByClassName('seat')
-    for (const seat of seats){
-        seat.addEventListener('click', function(){
-
-            createTr(seat.innerText)
-            seatLeft()
-            countSeat()
-            seatBackground(seat)
-            totalPrice()
-            seat.removeEventListener('click' , arguments.callee)
+    for (const seat of seats) {
+        seat.addEventListener('click', function () {
+            i = i + 1
+            if (i > 4) {
+                seat.removeEventListener('click', arguments.callee)
+            }
+            else {
+                createTr(seat.innerText)
+                seatLeft()
+                countSeat()
+                seatBackground(seat)
+                totalPrice()
+                seat.removeEventListener('click', arguments.callee)
+            }
+            if(i > 3){
+                enableApply()
+            }
         })
+
+        console.log(i)
     }
 }
 selectSeat()
